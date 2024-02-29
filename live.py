e87471819e8008600f9e49aec430ad4bd7a344d0
murtiuwu <152479678+murtiuwu@users.noreply.github.com>
2024-03-01 00:37:42 +0800

    жизнь с пользовательским интерфейсом

--- live.py
+++ live.py
@@ -4,15 +4,32 @@ import pygame
 
 '''
 запускайте этот код в таком состоянии чтобы посмотреть как он будет работать.
-я подписал комментариями все величины которые можно менять чтобы подстраивать поле 
-под себя. для генерации случайного поля по новой достаточно нажать любую кнопку на клавиатуре
-(например пробел) не закрывая окно с текущей генерацией
+я подписал комментариями все величины которые можно менять чтобы подстраивать поле под себя.
+
+пробел - режим строительства
+r - случайная генерация
+d - очистка всего поля (не работает во время режима строительства)
+s (во время режима строительства) - сохранение текущего поля в ячейку которую ты указываешь (1 символ)
+l (во время режима строительства) - загружает поле из указанной ячейки
+
+во время режима строительства 
+ЛКМ - поставить клетку
+ПКМ - удалить клетку
 '''
+
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+EAZY_WHITE = (230, 230, 230)
+
+BG_COLOR = BLACK
+
+# block_size = 10 # это размер одной клетки (в пикселях)
 
 pygame.init()
 
 pool = []
-for i in range(90): # размер поля по х (в клетках)
+for i in range(150): # размер поля по х (в клетках)
     pool.append(['.'] * 90) # размер поля по у (в клетках)
 
 # этот код случайно генерирует первое поколение если вас не устраивает планер в начале
@@ -24,14 +41,14 @@ for i in range(90): # размер поля по х (в клетках)
 def dead(Pool, X, Y):
     Life = 0
 
-    list_of_creature = [Pool[Y % (len(Pool) - 1)][(X - 1) % (len(Pool) - 1)]]
-    list_of_creature.append(Pool[Y% (len(Pool) - 1)][(X + 1) % (len(Pool) - 1)])
-    list_of_creature.append(Pool[(Y + 1) % (len(Pool) - 1)][(X + 1) % (len(Pool) - 1)])
-    list_of_creature.append(Pool[(Y + 1) % (len(Pool) - 1)][X % (len(Pool) - 1)])
-    list_of_creature.append(Pool[(Y + 1) % (len(Pool) - 1)][(X - 1) % (len(Pool) - 1)])
-    list_of_creature.append(Pool[(Y - 1) % (len(Pool) - 1)][(X + 1) % (len(Pool) - 1)])
-    list_of_creature.append(Pool[(Y - 1) % (len(Pool) - 1)][X % (len(Pool) - 1)])
-    list_of_creature.append(Pool[(Y - 1) % (len(Pool) - 1)][(X - 1) % (len(Pool) - 1)])
+    list_of_creature = [Pool[Y % (len(Pool) - 1)][(X - 1) % (len(Pool[0]) - 1)]]
+    list_of_creature.append(Pool[Y% (len(Pool) - 1)][(X + 1) % (len(Pool[0]) - 1)])
+    list_of_creature.append(Pool[(Y + 1) % (len(Pool) - 1)][(X + 1) % (len(Pool[0]) - 1)])
+    list_of_creature.append(Pool[(Y + 1) % (len(Pool) - 1)][X % (len(Pool[0]) - 1)])
+    list_of_creature.append(Pool[(Y + 1) % (len(Pool) - 1)][(X - 1) % (len(Pool[0]) - 1)])
+    list_of_creature.append(Pool[(Y - 1) % (len(Pool) - 1)][(X + 1) % (len(Pool[0]) - 1)])
+    list_of_creature.append(Pool[(Y - 1) % (len(Pool) - 1)][X % (len(Pool[0]) - 1)])
+    list_of_creature.append(Pool[(Y - 1) % (len(Pool) - 1)][(X - 1) % (len(Pool[0]) - 1)])
 
 
     for i in list_of_creature:
@@ -46,14 +63,14 @@ def dead(Pool, X, Y):
 def life(Pool, X, Y):
     Life = 0
 
-    list_of_creature = [Pool[Y % (len(Pool) - 1)][(X - 1) % (len(Pool) - 1)]]
-    list_of_creature.append(Pool[Y% (len(Pool) - 1)][(X + 1) % (len(Pool) - 1)])
-    list_of_creature.append(Pool[(Y + 1) % (len(Pool) - 1)][(X + 1) % (len(Pool) - 1)])
-    list_of_creature.append(Pool[(Y + 1) % (len(Pool) - 1)][X % (len(Pool) - 1)])
-    list_of_creature.append(Pool[(Y + 1) % (len(Pool) - 1)][(X - 1) % (len(Pool) - 1)])
-    list_of_creature.append(Pool[(Y - 1) % (len(Pool) - 1)][(X + 1) % (len(Pool) - 1)])
-    list_of_creature.append(Pool[(Y - 1) % (len(Pool) - 1)][X % (len(Pool) - 1)])
-    list_of_creature.append(Pool[(Y - 1) % (len(Pool) - 1)][(X - 1) % (len(Pool) - 1)])
+    list_of_creature = [Pool[Y % (len(Pool) - 1)][(X - 1) % (len(Pool[0]) - 1)]]
+    list_of_creature.append(Pool[Y% (len(Pool) - 1)][(X + 1) % (len(Pool[0]) - 1)])
+    list_of_creature.append(Pool[(Y + 1) % (len(Pool) - 1)][(X + 1) % (len(Pool[0]) - 1)])
+    list_of_creature.append(Pool[(Y + 1) % (len(Pool) - 1)][X % (len(Pool[0]) - 1)])
+    list_of_creature.append(Pool[(Y + 1) % (len(Pool) - 1)][(X - 1) % (len(Pool[0]) - 1)])
+    list_of_creature.append(Pool[(Y - 1) % (len(Pool) - 1)][(X + 1) % (len(Pool[0]) - 1)])
+    list_of_creature.append(Pool[(Y - 1) % (len(Pool) - 1)][X % (len(Pool[0]) - 1)])
+    list_of_creature.append(Pool[(Y - 1) % (len(Pool) - 1)][(X - 1) % (len(Pool[0]) - 1)])
 
 
     for i in list_of_creature:
@@ -71,22 +88,33 @@ pool[2][5] = '0'
 pool[2][6] = '0'
 pool[1][4] = '0'
 
+
+def convert_To_Matrix(test_str, K):
+    temp = [test_str[idx: idx + K] for idx in range(0, len(test_str), K)]
+    res = [list(ele) for ele in temp]
+    return res
+
 def play(pool):
-    block_size = 10 # это размер одной клетки (в пикселях)
+    block_size = 10  # это размер одной клетки (в пикселях)
     width = len(pool) * block_size
     height = len(pool[0]) * block_size
+
+    gun = pygame.image.load('/Users/timur/desktop/sprites/gun.jpg')
 
     clock = pygame.time.Clock()
 
     pygame_scene = pygame.display.set_mode((width, height))
     pygame.display.set_caption('live')
 
-    GREEN = (0,255,0)
-    BLACK = (0, 0, 0)
-    WHITE = (255,255,255)
-    EAZY_WHITE = (230,230,230)
-
     pause = False
+    save = False
+    load = False
+
+    in_del, out_del = "", ""
+
+    text = pygame.font.Font(None, 30)
+
+    give_me_save = text.render('введите номер файла сохранения', False, WHITE)
 
     while True:
         # r = random.randrange(0, 256)
@@ -111,9 +139,7 @@ def play(pool):
                 pygame.quit()
                 quit()
 
-
-
-        pygame_scene.fill(BLACK)
+        pygame_scene.fill(BG_COLOR)
         poll_res = []
         for i in pool:
             poll_res.append(copy.copy(i))
@@ -128,10 +154,42 @@ def play(pool):
                 else:
                     poll_res[cor_y][cor_x] = life(pool, cor_x, cor_y)
         pool = []
+
+
         while pause:
-            pygame_scene.fill(BLACK)
+
+            pygame_scene.fill(BG_COLOR)
             mouse_pos = pygame.mouse.get_pos()
             hes_a_live = pygame.mouse.get_pressed()
+            pygame_scene.blit(gun, (100, 100))
+
+            while load:
+                pygame_scene.blit(give_me_save, (10, 10))
+                pygame.display.update()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                    if event.type == pygame.KEYDOWN:
+                        with open(f'save_slot{event.unicode}', 'r') as save_slot:
+                            poll_res = convert_To_Matrix(save_slot.read(), len(poll_res[0]))
+                            load = False
+
+
+            while save:
+                pygame_scene.blit(give_me_save, (10,10))
+                pygame.display.update()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                    if event.type == pygame.KEYDOWN:
+                        with open(f'save_slot{event.unicode}', 'w') as save_slot:
+                            text = out_del.join([in_del.join([str(ele) for ele in sub]) for sub in poll_res])
+                            save_slot.write(str(text))
+                            save = False
+
+
             if hes_a_live[0]:
                 x = int(mouse_pos[0] / block_size)
                 y = int(mouse_pos[1] / block_size)
@@ -140,13 +198,19 @@ def play(pool):
                 x = int(mouse_pos[0] / block_size)
                 y = int(mouse_pos[1] / block_size)
                 poll_res[x][y] = '.'
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_l:
+                        load = True
+                    if event.key == pygame.K_s:
+                        save = True
                     if event.key == pygame.K_SPACE:
                         pause = False
+
             for row in range(len(poll_res)):
                 for column in range(len(poll_res[row])):
                     if poll_res[row][column] == '0':
@@ -159,6 +223,8 @@ def play(pool):
                         pygame.draw.rect(pygame_scene, EAZY_WHITE,
                                          (row * block_size, column * block_size, block_size, block_size))
             pygame.display.update()
+
+
         for i in poll_res:
             pool.append(copy.copy(i))
         for row in range(len(poll_res)):
@@ -173,8 +239,6 @@ def play(pool):
                     pygame.draw.rect(pygame_scene, EAZY_WHITE,
                                      (row*block_size, column*block_size, block_size, block_size))
         pygame.display.update()
-
-
 
         clock.tick(15)
 
